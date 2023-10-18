feebb43b7b57c442ff628ce35acd29a8e9c0e0c2
André Ripari <andrerip@gmail.com>
2023-10-18 15:01:43 -0300

    add Tailwind and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
